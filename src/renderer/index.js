58055d3cf4d3bc4d2ad94a79971183660a7603ff
Shawn M Moore <code@sartak.org>
2020-05-09 16:23:54 -0400

    Put App into its own file

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const App = () => {
-  return <h1>slate2</h1>;
-};
+import App from './App';
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
