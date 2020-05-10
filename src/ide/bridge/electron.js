1dce3d56594b25bb519e4657905a343241126160
Shawn M Moore <code@sartak.org>
2020-05-10 01:32:10 -0400

    Support for loading projects

--- src/ide/bridge/electron.js
+++ src/ide/bridge/electron.js
@@ -4,3 +4,54 @@ export const saveProject = (project) => {
   ipc.send('save-project', project);
 };
 
+let loadPromiseResolve = null;
+let loadPromiseReject = null;
+export const loadProject = (project) => {
+  if (loadPromiseResolve || loadPromiseReject) {
+    throw new Error('Inconsistent state: loadProject called before previous loadProject finished');
+  }
+
+  const loadPromise = new Promise((resolve, reject) => {
+    loadPromiseResolve = resolve;
+    loadPromiseReject = reject;
+    ipc.send('load-project');
+  });
+
+  return loadPromise;
+};
+
+ipc.on('load-project-success', (event, project) => {
+  if (!loadPromiseResolve) {
+    throw new Error('Inconsistent state: got load-project-success with no expected loadProject');
+  }
+
+  const resolve = loadPromiseResolve;
+  loadPromiseResolve = null;
+  loadPromiseReject = null;
+
+  resolve(project);
+});
+
+ipc.on('load-project-error', (event, err) => {
+  if (!loadPromiseReject) {
+    throw new Error('Inconsistent state: got load-project-error with no expected loadProject');
+  }
+
+  const reject = loadPromiseReject;
+  loadPromiseResolve = null;
+  loadPromiseReject = null;
+
+  reject(err);
+});
+
+ipc.on('load-project-cancel', (event) => {
+  if (!loadPromiseResolve) {
+    throw new Error('Inconsistent state: got load-project-cancel with no expected loadProject');
+  }
+
+  const resolve = loadPromiseResolve;
+  loadPromiseResolve = null;
+  loadPromiseReject = null;
+
+  resolve(null);
+});
--- src/shell/bridge.js
+++ src/shell/bridge.js
@@ -26,10 +26,54 @@ ipc.on('save-project', (event, project) => {
   }
 });
 
+ipc.on('load-project', (event) => {
+  const {sender} = event;
+
+  dialog.showOpenDialog(sender, {
+    filters: [
+      { name: 'slate2 Project', extensions: ['s2p'] },
+    ],
+    properties: {
+      openFile: true,
+      openDirectory: false,
+      multiSelections: true,
+      showHiddenFiles: false,
+      promptToCreate: true,
+    },
+  }).then(({canceled, filePaths}) => {
+    if (canceled || filePaths.length === 0) {
+      event.reply('load-project-cancel');
+    }
+    else {
+      const [filePath] = filePaths;
+      currentProjectFileForWindow.set(sender, filePath);
+      loadProject(filePath).then((project) => {
+        event.reply('load-project-success', project);
+      }).catch((err) => {
+        event.reply('load-project-error', err);
+      });
+    }
+  });
+});
+
 const saveProject = (project, filename) => {
   const data = JSON.stringify(project);
 
   // @Performance: this can be made asynchronous, though we'll want to
   // serialize concurrent saves
   fs.writeFileSync(filename, data);
+};
+
+const loadProject = (filename) => {
+  return new Promise((resolve, reject) => {
+    // @Compatibility: Windows can specify a nonexistent file here, which
+    // we should use as a signal to create the file rather than loading it.
+    fs.readFile(filename, (err, data) => {
+      if (err) {
+        reject(err);
+      }
+
+      resolve(JSON.parse(data));
+    });
+  });
 };
