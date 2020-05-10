cd384292686eca86f2814bd6d36db188bebc3afd
Shawn M Moore <code@sartak.org>
2020-05-09 20:31:52 -0400

    build-web-ide build target for our ide JS

--- webpack.ide.config.js
+++ webpack.ide.config.js
@@ -1,9 +1,17 @@
+const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
+const isBuildWebIDE = process.env.SLATE2_ENV === 'web';
 
 module.exports = {
   mode: isDevelopment ? 'development' : 'production',
+  ...(isBuildWebIDE ? {
+    entry: './src/ide/index.js',
+    output: {
+      path: path.join(__dirname, 'out', 'web-ide'),
+    },
+  } : null),
   resolve: {
     alias: {
       'react-dom': '@hot-loader/react-dom',
