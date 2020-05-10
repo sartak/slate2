6ceab69038329e6d947e2af97a450b19d06d3415
Shawn M Moore <code@sartak.org>
2020-05-10 01:32:10 -0400

    Plumbing for loading projects

--- src/ide/bridge/browser.js
+++ src/ide/bridge/browser.js
@@ -1,3 +1,8 @@
 export const saveProject = (project) => {
   alert('Unable to save project in the web version of slate2. You can still export the project file.');
 };
+
+export const loadProject = () => {
+  return Promise.resolve({});
+};
+
