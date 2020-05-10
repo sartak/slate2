651c6dc3a6f831700bfc0b3d2fbe0c66e955058b
Shawn M Moore <code@sartak.org>
2020-05-10 02:34:15 -0400

    Introduce 98.css

--- src/ide/App.js
+++ src/ide/App.js
@@ -3,7 +3,21 @@ import { hot } from 'react-hot-loader/root';
 import './App.less';
 
 const App = () => {
-  return <h1>slate2</h1>;
+  return (
+    <div class="window">
+      <div className="title-bar">
+        <div className="title-bar-text">slate2</div>
+        <div className="title-bar-controls">
+          <button aria-label="Minimize"></button>
+          <button aria-label="Maximize"></button>
+          <button aria-label="Close"></button>
+        </div>
+      </div>
+      <div className="window-body">
+        <h1>slate2</h1>
+      </div>
+    </div>
+  );
 };
 
 export default hot(App);
