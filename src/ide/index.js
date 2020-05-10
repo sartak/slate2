8deb4882d259abaeeed3d28455881ddd5e607a1a
Shawn M Moore <code@sartak.org>
2020-05-10 01:53:30 -0400

    Introduce redux

--- src/ide/index.js
+++ src/ide/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import App from './App';
+import { projectReducer } from './project';
+
+const projectStore = createStore(projectReducer);
 
 const render = (Component) => {
   ReactDOM.render(
     <AppContainer>
-      <Component />
+      <Provider store={projectStore}>
+        <App />
+      </Provider>
     </AppContainer>,
     document.getElementById('app'),
   );
@@ -20,5 +27,10 @@ if (module.hot) {
   module.hot.accept('./App', () => {
     const NextApp = require('./App').default;
     render(NextApp);
+  });
+
+  module.hot.accept('./project', () => {
+    const NextProject = require('./project');
+    projectStore.replaceReducer(NextProject.projectReducer);
   });
 }
---
+++ src/ide/project/index.js
@@ -0,0 +1,7 @@
+export const projectReducer = (state = null, action) => {
+  switch (action.type) {
+    default: {
+      return state;
+    }
+  }
+}
