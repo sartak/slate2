340138dc8d7c5d11588b35d8fea9baf7a442989e
Shawn M Moore <code@sartak.org>
2020-05-10 22:49:30 -0400

    UI for saving project

--- src/ide/ProjectEditor.js
+++ src/ide/ProjectEditor.js
@@ -1,14 +1,32 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { CodeEditor } from './CodeEditor';
+import { saveProject, canSaveProject } from '@ide/bridge';
 
 export const ProjectEditor = () => {
+  const [error, setError] = useState(null);
+
+  const [isSaving, setSaving] = useState(false);
+
   const dispatch = useDispatch();
   const project = useSelector(project => project);
   const code = useSelector(project => project.code);
 
+  const save = () => {
+    setSaving(true);
+    setError(null);
+
+    saveProject(project).then((saved) => {
+      setSaving(false);
+    }).catch((err) => {
+      setSaving(false);
+      setError(err);
+    });
+  }
+
   return (
     <div className="ProjectEditor" style={{padding: 20}}>
+      {error && <div className="error-banner">{error.toString()}</div>}
       <CodeEditor
         width="400"
         height="300"
@@ -16,6 +34,7 @@ export const ProjectEditor = () => {
         value={code}
         onChange={(code) => dispatch({type: 'change-code', code})}
       />
+      {canSaveProject && <button disabled={isSaving} onClick={save}>Save Project</button>}
     </div>
   );
 };
