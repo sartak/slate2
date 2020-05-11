49715525d94fe5da7e011da3a0a3c31e4fc7c2c0
Shawn M Moore <code@sartak.org>
2020-05-10 20:17:17 -0400

    Add monaco-vim

--- src/ide/CodeEditor.js
+++ src/ide/CodeEditor.js
@@ -1,21 +1,64 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 import MonacoEditor from 'react-monaco-editor';
+import { initVimMode } from 'monaco-vim';
+import './CodeEditor.less';
 
-export const CodeEditor = (props) => {
+export const CodeEditorBase = (props) => {
   return (
-    <div className="CodeEditor">
-      <MonacoEditor
-        theme="vs-dark"
+    <MonacoEditor
+      theme="vs-dark"
 
+      {...props}
+
+      options={{
+        minimap: {
+          enabled: false,
+        },
+        ...(props.options || null)
+      }}
+    />
+  );
+};
+
+const addVim = (editor, statusBar) => {
+  // @Performance: call vimMode.dispose() when the editor is finished
+  return initVimMode(editor, statusBar);
+};
+
+export const CodeEditorVim = (props) => {
+  const editorRef = useRef();
+
+  const statusBarRef = useCallback((statusBar) => {
+    if (editorRef.current) {
+      addVim(editorRef.current, statusBar);
+    }
+  }, []);
+
+  const editorDidMount = (editor, ...rest) => {
+    editorRef.current = editor;
+
+    if (statusBarRef.current) {
+      addVim(editor, statusBarRef.current);
+    }
+
+    if (props.editorDidMount) {
+      props.editorDidMount(editor, ...rest);
+    }
+  };
+
+  return (
+    <div className="CodeEditorVim">
+      <CodeEditorBase
         {...props}
-
-        options={{
-          minimap: {
-            enabled: false,
-          },
-          ...(props.options || null)
-        }}
+        editorDidMount={editorDidMount}
+      />
+      <div
+        style={{width: Number(props.width)}}
+        className="statusBar"
+        ref={statusBarRef}
       />
     </div>
   );
 };
+
+export const CodeEditor = CodeEditorVim;
