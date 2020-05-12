77b8ce060c9090e08e24a86db2eb0d3f1efa743a
Shawn M Moore <code@sartak.org>
2020-05-12 00:22:29 -0400

    Render a grid on the canvas

--- src/ide/renderer.js
+++ src/ide/renderer.js
@@ -19,6 +19,39 @@ const classes = {};
         this.render();
       }
     }
+
+    render() {
+      super.render();
+      this.drawGrid();
+    }
+
+    drawGrid() {
+      const { canvas, ctx } = this;
+      const { width, height } = canvas;
+
+      const size = 32;
+
+      ctx.strokeStyle = '#333';
+      ctx.resetTransform();
+      ctx.translate(0.5, 0.5); // crisper lines
+
+      const x0 = 0;
+      const x1 = width;
+      const y0 = 0;
+      const y1 = height;
+
+      for (let x = x0; x < x1; x += size) {
+        ctx.moveTo(x, y0);
+        ctx.lineTo(x, y1);
+        ctx.stroke();
+      }
+
+      for (let y = y0; y < y1; y += size) {
+        ctx.moveTo(x0, y);
+        ctx.lineTo(x1, y);
+        ctx.stroke();
+      }
+    }
   };
 
   classes[name] = dynamicClass;
