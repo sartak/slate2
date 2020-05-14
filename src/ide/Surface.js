1d953e8534f0b83db1199a49492e15c94adf15fe
Shawn M Moore <code@sartak.org>
2020-05-14 01:13:25 -0400

    When we select an entity, focus it into the view

--- src/ide/Surface.js
+++ src/ide/Surface.js
@@ -3,12 +3,27 @@ import { useDispatch, useSelector } from 'react-redux';
 import { commitSurfaceTransformAction } from './project';
 import { rendererForType } from './renderer';
 
+const useSelectedEntityChangeCallback = (callback, entities, selectedEntityIndex) => {
+  const prev = useRef(null);
+
+  useEffect(() => {
+    if (prev.current === null) {
+      prev.current = selectedEntityIndex;
+      return;
+    }
+
+    callback(entities[selectedEntityIndex]);
+  }, [selectedEntityIndex]);
+};
+
 export const Surface = () => {
   const dispatch = useDispatch();
   const rendererType = useSelector(project => project.renderer);
   const surfaceOpts = useSelector(project => project.surface);
   const rendererRef = useRef(null);
   const surfaceRef = useRef(null);
+  const entities = useSelector(project => project.entities);
+  const selectedEntityIndex = useSelector(project => project.selectedEntityIndex);
 
   const surfaceCallback = useCallback((surface) => {
     surfaceRef.current = surface;
@@ -57,6 +72,20 @@ export const Surface = () => {
   if (rendererRef.current) {
     rendererRef.current.changeTransform(surfaceOpts);
   }
+
+  useSelectedEntityChangeCallback((entity) => {
+    entity.components.forEach((entityComponent) => {
+      if (entityComponent.name === 'TransformComponent') {
+        const {x, y} = entityComponent.fields;
+        dispatch(commitSurfaceTransformAction({
+          ...surfaceOpts,
+          panX: x,
+          panY: y,
+          zoom: 1,
+        }));
+      }
+    });
+  }, entities, selectedEntityIndex);
 
   return (
     <div className="Surface" ref={surfaceCallback} />
