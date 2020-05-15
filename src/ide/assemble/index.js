149dc33267b8a5e2d15b2f877f0214b69f613adb
Shawn M Moore <code@sartak.org>
2020-05-14 20:58:30 -0400

    Split assembleProject from assembleGame

--- src/ide/assemble/index.js
+++ src/ide/assemble/index.js
@@ -1,4 +1,4 @@
-import assembleProject from './project';
+import { assembleProject } from './project';
 import defaultIndexHtml from '!!raw-loader!./default-index.html';
 import defaultWebpackConfig from '!!raw-loader!./default-webpack.config.js';
 
--- src/ide/assemble/project.js
+++ src/ide/assemble/project.js
@@ -1,12 +1,18 @@
-export default (project) => {
+export const assembleProject = (project) => {
   return `
     import Game from '@slate2/game';
     import Renderer from '@slate2/renderer/${project.renderer}';
 
+    ${assembleGame(project)}
+  `;
+};
+
+export const assembleGame = (project) => {
+  return `
     export default new Game({
       renderer: Renderer,
       init: () => { },
       update: (dt, time) => { },
     });
   `;
-};
+}
