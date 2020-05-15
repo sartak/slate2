63db4998f5ef1758bbefa964c4fee6aaf621e9b3
Shawn M Moore <code@sartak.org>
2020-05-15 19:27:18 -0400

    Render index.html with handlebars

--- src/ide/assemble/index.js
+++ src/ide/assemble/index.js
@@ -1,10 +1,17 @@
 import { assembleGame } from './game';
 import defaultIndexHtml from '!!raw-loader!./default-index.html';
 import defaultWebpackConfig from '!!raw-loader!./default-webpack.config.js';
+import Handlebars from 'handlebars';
+
+export const assembleIndexHtml = (project) => {
+  const content = defaultIndexHtml;
+  const template = Handlebars.compile(content);
+  return template(project);
+};
 
 export const assembleProject = (project) => {
   return {
-    indexHtml: defaultIndexHtml,
+    indexHtml: assembleIndexHtml(project),
     webpackConfig: defaultWebpackConfig,
     game: assembleGame(project),
   };
