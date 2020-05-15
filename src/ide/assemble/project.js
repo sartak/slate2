db76e3ef36a5b04ad49aed1131034bcd07407549
Shawn M Moore <code@sartak.org>
2020-05-14 22:39:36 -0400

    Assemble components

--- src/ide/assemble/project.js
+++ src/ide/assemble/project.js
@@ -1,3 +1,5 @@
+import { ComponentByName } from '../project/components';
+
 export const assembleProject = (project) => {
   return `
     ${assembleImports(project)}
@@ -39,15 +41,71 @@ export const assembleECSImports = (project) => {
 };
 
 export const assembleECS = (project) => {
+  const entityComponentsForComponent = {};
   const indexForEntity = {};
 
   project.entities.forEach((entity, i) => {
     indexForEntity[entity.__id] = 1 + i;
+
+    entity.components.forEach((entityComponent) => {
+      const { name } = entityComponent;
+      if (!entityComponentsForComponent[entityComponent.name]) {
+        entityComponentsForComponent[entityComponent.name] = {
+          entities: {},
+        };
+      }
+
+      entityComponentsForComponent[entityComponent.name].entities[entity.__id] = entityComponent;
+    });
+  });
+
+  const components = [];
+  Object.entries(entityComponentsForComponent).forEach(([componentName, { entities }]) => {
+    const component = ComponentByName[componentName];
+    const componentFields = [];
+
+    component.fields.forEach(({name: fieldName, type, default: defaultValue}) => {
+      let zeroValue;
+
+      switch (type) {
+        case 'entity':
+        case 'float':
+          zeroValue = 0;
+          break;
+        case 'color':
+          zeroValue = '#000000';
+          break;
+        default:
+          throw new Error(`Unhandled type ${type} for zero value`);
+      }
+
+      const values = [zeroValue];
+
+      project.entities.forEach(({__id}, i) => {
+        if (!entities[__id]) {
+          values.push(zeroValue);
+          return;
+        }
+
+        let value = entities[__id].fields[fieldName];
+        values.push(value);
+      });
+
+      componentFields.push([fieldName, values]);
+    });
+    components.push([componentName, componentFields]);
   });
 
   return `
     entities: [${project.entities.map(({ __id }) => indexForEntity[__id]).join(', ')}],
-    components: [],
+    components: [${components.map(([componentName, fields]) => {
+      return `new ${componentName}Component({
+        ${fields.map(([fieldName, values]) => {
+          // @Performance: use ArrayBuffer?
+          return `  ${fieldName}: ${JSON.stringify(values)},\n`;
+        }).join("")}
+      })\n`;
+    }).join(', ')}],
     systems: {}
   `;
 }
