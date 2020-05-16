afd2d92c759b61929b430c1b228e8ed3487f8364
Shawn M Moore <code@sartak.org>
2020-05-16 01:26:59 -0400

    Preflight assembly

---
+++ src/ide/assemble/preflight.js
@@ -0,0 +1,33 @@
+import { assembleECSSetup as __assembleECSSetup, newContext as __newContext } from './game';
+import { ComponentByClassName as __ComponentClasses, SystemByClassName as __SystemClasses } from '../project/ecs';
+
+const __assembleGameForPreflight = (project) => {
+  const context = __newContext(project, {
+    prefix: '__',
+    componentClassPrefix: '__componentClasses.',
+    systemClassPrefix: '__systemClasses.',
+  });
+  const ecsSetup = __assembleECSSetup(project, context);
+  
+  return [
+    `(${context.rendererVar}) => {`,
+      ecsSetup,
+      `return {`,
+        `entities: ${context.entitiesVar},`,
+        `components: ${context.componentsVar},`,
+        `systems: ${context.systemsVar},`,
+        `init: () => { ${context.init.join("\n")} },`,
+        `update: (dt, time) => { ${context.update.join("\n")} },`,
+        `render: (dt, time) => { ${context.render.join("\n")} },`,
+      `};`,
+    `}`,
+  ].join("\n");
+};
+
+const __evaluateGameForPreflight = (__project) => {
+  const __componentClasses = __ComponentClasses;
+  const __systemClasses = __SystemClasses;
+  return eval(__assembleGameForPreflight(__project));
+};
+
+export { __evaluateGameForPreflight as evaluateGameForPreflight, __assembleGameForPreflight as assembleGameForPreflight };
