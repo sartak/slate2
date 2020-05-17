b364240b7b0c788c28a23401a127be6d008b097c
Shawn M Moore <code@sartak.org>
2020-05-17 00:29:28 -0400

    More selectors

--- src/ide/project/selectors.js
+++ src/ide/project/selectors.js
@@ -7,3 +7,39 @@ export const selectEnabledComponents = (state) => {
 export const selectEnabledSystems = (state) => {
   return [...BuiltinSystems];
 };
+
+const _selectComponentWithIdMemo = {};
+export const makeSelectComponentWithId = (componentId) => {
+  return (state) => {
+    if (!_selectComponentWithIdMemo[componentId]) {
+      const builtinComponent = BuiltinComponents.find((component) => component.id === componentId);
+      if (builtinComponent) {
+        _selectComponentWithIdMemo[componentId] = builtinComponent;
+      } else {
+        // construct UserDefinedComponent from state
+      }
+    }
+    return _selectComponentWithIdMemo[componentId];
+  };
+};
+
+const _selectSystemWithIdMemo = {};
+export const makeSelectSystemWithId = (systemId) => {
+  return (state) => {
+    if (!_selectSystemWithIdMemo[systemId]) {
+      const builtinSystem = BuiltinSystems.find((system) => system.id === systemId);
+      if (builtinSystem) {
+        _selectSystemWithIdMemo[systemId] = builtinSystem;
+      } else {
+        // construct UserDefinedSystem from state
+      }
+    }
+    return _selectSystemWithIdMemo[systemId];
+  };
+};
+
+export const makeSelectEntityComponent = (entityIndex, componentId) => {
+  return ({ entities }) => entities[entityIndex].componentConfig[componentId];
+};
+
+export const selectPreflightRunning = ({ preflightRunning }) => preflightRunning;
