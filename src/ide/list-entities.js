9f159436379da117927738b560b63bbcac1986fc
Shawn M Moore <code@sartak.org>
2020-05-22 16:09:42 -0400

    Factor out a list-objects

--- src/ide/list-entities.js
+++ src/ide/list-entities.js
@@ -3,7 +3,7 @@ import { useDispatch, useSelector, shallowEqual } from 'react-redux';
 import { addEntityAction, setActiveEntityAction } from './project/actions';
 import { newEntity } from './ecs/entities';
 import { selectEntityList, selectActiveEntityId } from './project/selectors';
-import './list-entities.less';
+import { ObjectList } from './list-objects';
 
 export const EntityList = () => {
   const dispatch = useDispatch();
@@ -17,22 +17,12 @@ export const EntityList = () => {
   const activeEntityId = useSelector(selectActiveEntityId);
 
   return (
-    <div className="EntityList">
-      <ul>
-        {entities.map((entity) => {
-          const { id, label } = entity;
-          return (
-            <li
-              key={id}
-              className={activeEntityId === id ? "active" : null}
-              onClick={() => dispatch(setActiveEntityAction(id))}
-            >{label}</li>
-          );
-        })}
-      </ul>
-      <div className="controls">
-        <button onClick={() => dispatch(addEntityAction(newEntity(0, 0)))}>Add Entity</button>
-      </div>
-    </div>
+    <ObjectList
+      objects={entities}
+      activeId={activeEntityId}
+      onSelect={({ id }) => dispatch(setActiveEntityAction(id))}
+      onAdd={() => dispatch(addEntityAction(newEntity(0, 0)))}
+      addLabel="Add Entity"
+    />
   );
 }
---
+++ src/ide/list-objects.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './list-objects.less';
+
+export const ObjectList = ({ objects, activeId, onSelect, onAdd, addLabel }) => {
+  return (
+    <div className="ObjectList">
+      <ul>
+        {objects.map((object) => {
+          const { id, label } = object;
+          return (
+            <li
+              key={id}
+              className={activeId === id ? "active" : null}
+              onClick={() => onSelect(object)}
+            >{label}</li>
+          );
+        })}
+      </ul>
+      <div className="controls">
+        <button onClick={onAdd}>{addLabel}</button>
+      </div>
+    </div>
+  );
+};
