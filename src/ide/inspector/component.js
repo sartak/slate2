894000a19cf7eb9853f110de321e75f0e8690e3a
Shawn M Moore <code@sartak.org>
2020-05-22 18:24:41 -0400

    Allow editing component and system labels

--- src/ide/inspector/component.js
+++ src/ide/inspector/component.js
@@ -1,13 +1,42 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { setUserDefinedComponentLabelAction } from '../project/actions';
 import { makeSelectComponent } from '../project/selectors';
+import { TextControlled } from '../field/text-controlled';
+
+export const InspectUserDefinedComponentLabel = ({ id }) => {
+  const dispatch = useDispatch();
+  const component = useSelector(
+    makeSelectComponent(id),
+    (prev, next) => prev.label === next.label,
+  );
+
+  const { label } = component;
+  const setLabel = (label) => dispatch(setUserDefinedComponentLabelAction(id, label));
+
+  return (
+    <div className="InspectLabel">
+      <TextControlled
+        value={label}
+        defaultValue={`Component${id}`}
+        onChange={setLabel}
+      />
+    </div>
+  );
+};
 
 export const InspectComponent = ({ id }) => {
   const component = useSelector(makeSelectComponent(id));
 
+  const { userDefined } = component;
+
   return (
     <div className="InspectObject">
-      <div className="label">{component.label}</div>
+      { userDefined ? (
+        <InspectUserDefinedComponentLabel id={id} />
+      ) : (
+        <div className="label">{component.label}</div>
+      )}
     </div>
   );
 };
--- src/ide/inspector/system.js
+++ src/ide/inspector/system.js
@@ -1,13 +1,42 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { setUserDefinedSystemLabelAction } from '../project/actions';
 import { makeSelectSystem } from '../project/selectors';
+import { TextControlled } from '../field/text-controlled';
+
+export const InspectUserDefinedSystemLabel = ({ id }) => {
+  const dispatch = useDispatch();
+  const system = useSelector(
+    makeSelectSystem(id),
+    (prev, next) => prev.label === next.label,
+  );
+
+  const { label } = system;
+  const setLabel = (label) => dispatch(setUserDefinedSystemLabelAction(id, label));
+
+  return (
+    <div className="InspectLabel">
+      <TextControlled
+        value={label}
+        defaultValue={`System${id}`}
+        onChange={setLabel}
+      />
+    </div>
+  );
+};
 
 export const InspectSystem = ({ id }) => {
   const system = useSelector(makeSelectSystem(id));
 
+  const { userDefined } = system;
+
   return (
     <div className="InspectObject">
-      <div className="label">{system.label}</div>
+      { userDefined ? (
+        <InspectUserDefinedSystemLabel id={id} />
+      ) : (
+        <div className="label">{system.label}</div>
+      )}
     </div>
   );
 };
