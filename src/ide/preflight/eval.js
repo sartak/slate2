03229e825a93ae90d6c0b1069baf98eb146456a6
Shawn M Moore <code@sartak.org>
2020-05-23 19:06:50 -0400

    Add $c, $s, $cm, and $sm

--- src/ide/preflight/eval.js
+++ src/ide/preflight/eval.js
@@ -1,18 +1,53 @@
-import { selectActiveEntityId } from '../project/selectors';
+import { selectActiveTypeId } from '../project/selectors';
 
 export default class EvalDebugger {
   project = null;
   assembly = null;
   preflightRunning = false;
 
-  eval(code) {
-    const { project, assembly, preflightRunning } = this;
-    const { entityMap } = assembly.context;
+  prepareParams() {
+    const { project, assembly } = this;
+    const { entities, components, systems } = assembly;
+    const { entityMap, componentObjects } = assembly.context;
 
-    const activeEntityId = selectActiveEntityId(project);
-    const params = {
-      '$e': activeEntityId ? entityMap[activeEntityId].index : null,
+    const [activeType, activeId] = selectActiveTypeId(project);
+
+    let $c = null;
+    const $cm = {};
+    componentObjects.forEach((component) => {
+      const { id: componentId, label: componentLabel, fields } = component;
+      const fieldsByLabel = $cm[componentLabel] = {};
+      const fieldsById = components[componentId];
+
+      component.fields.forEach(({ id, label }) => {
+        fieldsByLabel[label ?? id] = fieldsById[id];
+      });
+
+      if (activeType === 'Component' && activeId === componentId) {
+        $c = fieldsByLabel;
+      }
+    });
+
+    const $sm = {};
+    Object.entries(systems).forEach(([id, system]) => {
+      $sm[system.label] = system;
+    });
+
+    return {
+      $e: activeType === 'Entity' ? entityMap[activeId].index : null,
+      $c,
+      $s: activeType === 'System' ? systems[activeId] : null,
+
+      $es: entities,
+      $cm,
+      $sm,
     };
+  }
+
+  eval(code) {
+    const { assembly, preflightRunning } = this;
+
+    const params = this.prepareParams();
 
     if (preflightRunning) {
       assembly.scheduleEval(code, params);
