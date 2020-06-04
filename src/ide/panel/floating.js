99698e1da8555c93d59bc1837b503bb115bc39f3
Shawn M Moore <code@sartak.org>
2020-06-04 00:07:04 -0400

    Provide close context to all panel floating descendents

--- src/ide/panel/floating.js
+++ src/ide/panel/floating.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { createContext, useContext } from 'react';
 import './floating.less';
+
+const PanelFloatingContext = createContext(null);
+export const useClosePanelFloating = () => useContext(PanelFloatingContext);
 
 export const PanelFloating = ({ close, children }) => {
   return (
     <div className="PanelFloating">
       <button className="close" onClick={close}>x</button>
-      {children}
+      <PanelFloatingContext.Provider value={close}>
+        {children}
+      </PanelFloatingContext.Provider>
     </div>
   );
 };
--- src/ide/panel/index.js
+++ src/ide/panel/index.js
@@ -3,4 +3,4 @@ export { PanelRight } from './right';
 export { PanelBottom } from './bottom';
 export { PanelLeft } from './left';
 export { PanelCenter } from './center';
-export { PanelFloating } from './floating';
+export { PanelFloating, useClosePanelFloating } from './floating';
